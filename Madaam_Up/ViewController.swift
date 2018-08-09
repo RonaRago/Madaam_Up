c81d65326d6a8b2f1435c60e126dcea35d1c3d64
IOS DEV <IOS.DEV@mail.com>
2018-08-10 00:49:27 +0800

    initial commit

---
+++ Madaam_Up/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Madaam_Up
+//
+//  Created by Rona Rago on 10/08/2018.
+//  Copyright © 2018 Rona Rago. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
